ff9b3fd1a95c68ae112b6bb7372619ca6f280775
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-08-08 17:54:20 -0400

    add simple authentication

--- schemas.py
+++ schemas.py
@@ -1,10 +1,14 @@
 from marshmallow import Schema, fields
 
 class RegisterSchema(Schema):
-    name = fields.String(required=True)
-    email = fields.Email(required=True)
-    password = fields.String(required=True)
+    name = fields.String(required=True, load_only=True)
+    email = fields.Email(required=True, load_only=True)
+    password = fields.String(required=True, load_only=True)
 
 class LoginSchema(Schema):
-    name = fields.String(required=True)
-    password = fields.String(required=True)
+    name = fields.String(required=True, load_only=True)
+    password = fields.String(required=True, load_only=True)
+
+class LoginSuccessSchema(Schema):
+    access_token = fields.String(required=True)
+
